a1c5a829595a26f1f0e348650abc890f6bffff6a
asif riad <asifriad15@gmail.com>
2022-05-25 12:00:30 +0600

    add to find one products  functionality

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { get } = require('express/lib/response');
 const app = express()
@@ -11,7 +12,7 @@ app.use(express.json());
 // useername: Computer-Accessories
 // pas: 5ofXvFglXI7XWVsm
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jex3yne.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -19,6 +20,8 @@ async function run() {
     try {
         await client.connect();
         const allProducts = client.db('computer_accessories').collection('Products');
+
+        // find all products
         app.get('/products', async (req, res) => {
             const query = {};
             const cursor = allProducts.find(query);
@@ -26,6 +29,16 @@ async function run() {
             res.send(products);
 
         });
+
+        // find one products
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const product = await allProducts.findOne(query);
+            res.send(product);
+        });
+
+
     } finally {
 
     }
